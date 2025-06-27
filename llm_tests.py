3df9e8bdf87a2964c0bb7740091f014ac827afdd
Vic P <vic4key@gmail.com>
2025-06-28 02:28:10 +0700

    Initial

--- llm_tests.py
+++ llm_tests.py
@@ -15,10 +15,20 @@ Pack swimsuit in 1 days
 """)
 print(result)
 
+result = llm_gen_task_list("""
+Trong một tuần làm việc, tôi thường bắt đầu mỗi buổi sáng với 30 phút kiểm tra email và cập nhật tiến độ nhóm, sau đó dành 3 tiếng cho các công việc như soạn thảo văn bản và xử lý hồ sơ.
+Mỗi tuần, tôi có 2 buổi dành 1 tiếng 30 phút để gọi điện trao đổi với khách hàng về dự án.
+Sau giờ nghỉ trưa 1 giờ, tôi dành 30 phút giờ để cập nhật dữ liệu vào hệ thống, 2 tiếng rưỡi xử lý các vấn đề phát sinh, và 30 phút  cuối ngày để tổng hợp báo cáo.
+Vào cuối tuần, tôi dành 4 tiếng làm và đọc tài liệu chuyên môn, 1 giờ sắp xếp lại công việc, và 1 ngày để nghỉ ngơi thư giãn.
+""")
+print(result)
+
 
 
 """
 $ python llm_tests.py
+🤖 Using the LLM provider 'Local AI' with the model 'auto' at 'http://localhost:1234/v1/'
 ['task 1, 2 hrs', 'task 2, 4 hrs', 'task 3, 6 hrs']
-['do laundry, 1 hrs', 'cancel milk delivery, 0.5 hrs', 'clean fridge, 2 hrs', 'check passport, 0.5 hrs', 'do web check-in, 4 hrs', 'download movie for flight, 0.75 hrs', 'recharge mobile, 0.25 hrs', 'pack swimsuit, 24 hrs']
+['Do my laundry, 1 hrs', 'Cancel milk delivery, 0.5 hrs', 'Clean fridge, 2 hrs', 'Check passport, 0.5 hrs', 'Do web check-in, 4 hrs', 'Download a movie for the flight, 0.75 hrs', 'Recharge mobile, 0.25 hrs', 'Pack swimsuit, 24 hrs']
+['kiểm tra email và cập nhật tiến độ nhóm, 0.5 hrs', 'soạn thảo văn bản và xử lý hồ sơ, 3 hrs', 'gọi điện trao đổi với khách hàng về dự án, 1.5 hrs', 'cập nhật dữ liệu vào hệ thống, 0.5 hrs', 'xử lý các vấn đề phát sinh, 2.5 hrs', 'tổng hợp báo cáo, 0.5 hrs', 'làm và đọc tài liệu chuyên môn, 4 hrs', 'sắp xếp lại công việc, 1 hrs', 'nghỉ ngơi thư giãn, 24 hrs']
 """
